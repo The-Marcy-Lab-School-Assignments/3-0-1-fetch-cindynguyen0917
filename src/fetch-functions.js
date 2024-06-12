41d771b10db545f9ff0481cb07be1acdf4d0056f
Cindy Nguyen <cnguyen5138@gmail.com>
2024-06-11 20:53:31 -0400

    first short answer, some fetch functions but stuck on getting user posts

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,13 +1,47 @@
 const userUrl = 'https://jsonplaceholder.typicode.com/users'
 
 export const checkResponseStatus = () => {
+    return fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+        console.log(response.url)
+        console.log(response.status)
+        console.log(response.ok)
+        return { url: response.url, status: response.status, ok: response.ok }
+    })
 };
 
 export const getUsers = () => {
+    return fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+        console.log(response.body)
+        return response.json().then((users) => {
+            return users
+            // const users = []
+            // for (const person of responseData) {
+            //     console.log(person)
+            //     users.push({ id: person.id, usename: person.username })
+
+            // }
+            // console.log(users)
+            // return users
+        })
+    })
 };
 
-export const getUserPosts = () => {
-};
+export const getUserPosts = (userId, maxNumPosts = 3) => {
+    return fetch('https://jsonplaceholder.typicode.com/users/{userId}/posts').then((response) => {
+        // console.log(response.ok)
+        console.log(response.url)
+        return response.json().then((posts) => {
+            // console.log(posts)
+            const userPosts = posts.filter((post) => post.userId === userId)
+            return userPosts
+            // for (const person of posts) {
+            //     if (person[id] === userId) {
+            //         return person.posts
+            //     }
+            // }
+        })
+    })
+}
 
 export const createNewUser = () => {
 }
