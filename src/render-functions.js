b9d235bbdea96326bf663345942e11f74ce862ab
Cindy Nguyen <cnguyen5138@gmail.com>
2024-06-12 20:26:10 -0400

    done with rendering section

--- src/render-functions.js
+++ src/render-functions.js
@@ -1,5 +1,5 @@
 export const setupPageBasics = (parentEl) => {
-    parentEl.innerHTML = `
+  parentEl.innerHTML = `
       <h1>Intro To Fetch!</h1>
       <div id='status'></div>
       <div id='users'>
@@ -21,23 +21,64 @@ export const setupPageBasics = (parentEl) => {
       <div id='new-user'></div>
     `;
 
-    const statusDiv = parentEl.querySelector('#status');
-    const usersUl = parentEl.querySelector('#users-list');
-    const postsUl = parentEl.querySelector('#posts-list');
-    const newUserForm = parentEl.querySelector('#new-user-form');
-    const newUserDiv = parentEl.querySelector('#new-user');
+  const statusDiv = parentEl.querySelector('#status');
+  const usersUl = parentEl.querySelector('#users-list');
+  const postsUl = parentEl.querySelector('#posts-list');
+  const newUserForm = parentEl.querySelector('#new-user-form');
+  const newUserDiv = parentEl.querySelector('#new-user');
 
-    return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
+  return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
 };
 
-export const renderStatus = () => {
+export const renderStatus = (statusDiv, statusInfoObj) => {
+  const h2 = document.createElement('h2')
+  const p = document.createElement('p')
+
+  h2.id = "status-heading"
+  p.id = "status-code"
+
+  h2.textContent = `Info on - ${statusInfoObj.url}`
+  if (statusInfoObj.ok) {
+    p.textContent = `Status code: ${statusInfoObj.status}, OK!`
+  } else {
+    p.textContent = `Status code: ${statusInfoObj.status}, FAIL!`
+  }
+  statusDiv.append(h2, p)
 }
 
-export const renderUsers = () => {
+export const renderUsers = (usersUI, users) => {
+  usersUI.textContent = ''
+  users.forEach((user) => {
+    const li = document.createElement('li')
+    const button = document.createElement('button')
+    li.classList.add('user-card')
+    button.setAttribute('data-user-id', user.id)
+    button.textContent = `Load ${user.username}'s posts`
+    li.append(button)
+    usersUI.append(li)
+
+  })
 };
 
-export const renderPosts = () => {
+export const renderPosts = (postsUI, posts) => {
+  postsUI.textContent = ''
+  posts.forEach((post) => {
+    const li = document.createElement('li')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p')
+    h2.textContent = post.title
+    p.textContent = post.body
+    li.append(h2, p)
+    postsUI.append(li)
+
+  })
 }
 
-export const renderNewUser = () => {
+export const renderNewUser = (newUserDiv, newUserInfo) => {
+  newUserDiv.textContent = ''
+  const h2 = document.createElement('h2')
+  const p = document.createElement('p')
+  h2.textContent = newUserInfo.username
+  p.textContent = newUserInfo.email
+  newUserDiv.append(h2, p)
 }
